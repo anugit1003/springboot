a2afff0ea63bd77e6b4b50b5f027953cb16a11d5
anugituser-at-139751280652 <anuradhav1003@gmail.com>
2023-04-23 17:46:06 -0400

    updated-1

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -2,14 +2,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @EnableAutoConfiguration
 public class MyApplication {
 
-  
+  private static final Logger logger = LoggerFactory.getLogger(MyApplication.class);
   public static void main(String[] args) {
-   System.out.println("Anu testing"); 
+    
+   logger.info("Anu testing"); 
   SpringApplication.run(MyApplication.class, args);
   }
 
